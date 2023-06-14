5fcd9c89b90189c260008e1d5ddc60ea1bf65169
Victor Trevisan <victor.trevisan@snowmanlabs.com>
2023-06-14 11:00:34 -0300

    visual changes to BarberShop and cadastros Servico and Cliente

--- backend/database/mongo.js
+++ backend/database/mongo.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const url = 'mongodb://127.0.0.1:27017/barbearia';
+const url = 'mongodb://0.0.0.0:27017/barbearia';
 
 const db = mongoose.connect(url)
     .then(() => console.log('Conectado ao MongoDB'))
